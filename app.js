d2bc67e8d2593e664648f6729f7f6e11d2fbe6f7
Galen Corey <galen.corey@gmail.com>
2016-08-02 15:09:16 -0400

    logs method and route

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require( 'express' );
+var chalk = require('chalk');
 var app = express();
 
 app.listen(3000, function(){console.log("hi")});
+
+app.use('/',function(req, res){
+  var method = req.method;
+  var route = req.originalUrl;
+
+  console.log(method, route);
+});
+
 app.get('/',function(req, res){
   res.send("Welcome!");
 });
+
