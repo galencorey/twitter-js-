97d68f94fa05fd702b09b73725ff902544c6b35d
Galen Corey <galen.corey@gmail.com>
2016-08-02 15:21:17 -0400

    added special route

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var app = express();
 
 app.listen(3000, function(){console.log("hi")});
 
-app.use('/',function(req, res){
-  var method = req.method;
-  var route = req.originalUrl;
+app.use('/',function(req, res, next){
+  var method = chalk.blue(req.method);
+  var route = chalk.red(req.originalUrl);
 
   console.log(method, route);
+  next();
+});
+
+app.use('/special', function(req, res, next){
+  console.log('you reached the special area');
 });
 
 app.get('/',function(req, res){
