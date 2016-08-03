4269b55db9c7066a8d0f5b885aa3c7d05d7f8db9
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2016-08-03 10:28:47 -0400

    trying to commit...

--- app.js
+++ app.js
@@ -3,20 +3,26 @@ var chalk = require('chalk');
 var swig = require('swig');
 var app = express();
 var routes = require('./routes');
+var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views'); // point res.render to the proper directory
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', swig.renderFile); // when giving html files to res.render, tell it to use swig
 swig.setDefaults({ cache: false });
 
-app.listen(3000, function(){console.log("hi")});
+app.listen(3000, function(){
+  console.log('Listening on port 3000...')
+});
 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static('public'));
-
 app.use('/', function(req, res, next){
   var method = chalk.blue(req.method);
   var route = chalk.red(req.originalUrl);
-
   console.log(method, route);
   next();
 });
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/stylesheets/style.css', function(req, res){
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name', function(req, res) {
@@ -22,6 +22,13 @@ router.get('/tweets/:id', function(req, res) {
   var id = parseInt(req.params.id);
   var tweets = tweetBank.find({id: id});
   res.render( 'index', { tweets: tweets } );
+});
+
+router.post('/tweets', function (req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 module.exports = router;
